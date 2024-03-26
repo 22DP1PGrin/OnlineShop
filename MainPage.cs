a2972d5869156e354e1ddb01701f2c2fa73a9ae2
22DP1PGrin <131352980+22DP1PGrin@users.noreply.github.com>
2024-03-26 12:07:03 +0200

    Create MainPage.cs

---
+++ MainPage.cs
@@ -0,0 +1,10 @@
+public class MainPage
+{
+    public void Methods()
+    {
+        Registration obj = new Registration();
+        Store store = new Store();
+        obj.Method1();
+        store.DisplayStore();
+    }
+}
