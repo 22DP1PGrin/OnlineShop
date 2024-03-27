a35ee49f967e275df2e7408cec46deb5675e1c01
22DP1PGrin <131352980+22DP1PGrin@users.noreply.github.com>
2024-03-27 15:03:08 +0200

    Add files via upload

---
+++ LogIn.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+
+public class LogIn
+{
+    string email = "";
+    bool PasswordExists = false;
+
+    string path = @"C:\Users\pgrin\source\repos\Eksamens\ConsoleApp3\Accounts.csv";
+    //string path = @"C:\Users\Admin\source\repos\Eksamens\Eksamens\Accounts.csv";
+
+    public void Log()
+    {
+        while(true)
+        {
+            Console.WriteLine("Enter your email! ");
+            email = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        if (parts.Contains(email))
+                        {
+                            Console.WriteLine("Enter your password: ");
+                            string password = Console.ReadLine();
+                            if (parts.Contains(password))
+                            {
+                                PasswordExists = true;
+                            }
+                            else
+                            {
+                                PasswordExists = false;
+                            }
+                        }
+                    }
+                }
+                if (!PasswordExists)
+                {
+                    Console.WriteLine("Invalid email or password. Try again!");
+                    Registration obj = new Registration();
+                    obj.Register();
+                }
+                else
+                {
+
+                    Console.WriteLine("You are logged in!");
+                    break;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ UserTest.cs
@@ -0,0 +1,146 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+public class UserTest
+{
+    string Name, Surname, PhoneNumber, Email, Password, Password2;
+    string path = @"C:\Users\pgrin\source\repos\Eksamens\ConsoleApp3\Accounts.csv";
+    //string path = @"C:\Users\Admin\source\repos\Eksamens\Eksamens\Accounts.csv";
+
+    File file = new File();
+
+
+    //Pārbauda lietotājvārda rakstzīmes, izņemot burtus, un neiziet no сikla, kamēr vards nav pareizs.
+    public void name()
+    {
+        do
+        {
+            Console.WriteLine("Enter your name: ");
+            Name = Console.ReadLine();
+            if (!Regex.IsMatch(Name, @"^[a-zA-Z]+$"))
+            {
+                Console.WriteLine("Invalid name. Try again!");
+            }
+            else
+            {
+                file.Writer(Name);
+                break;
+            }
+        } while (true);
+    }
+    //Pārbauda lietotājuzvārda rakstzīmes, izņemot burtus, un neiziet no сikla, kamēr uzvards nav pareizs.
+    public void surname()
+    {
+        do
+        {
+            Console.WriteLine("Enter your surname: ");
+            Surname = Console.ReadLine();
+            if (!Regex.IsMatch(Surname, @"^[a-zA-Z]+$"))
+            {
+                Console.WriteLine("Invalid surname. Try again!");
+            }
+            else
+            {
+                file.Writer(Surname);
+                break;
+            }
+        } while (true);
+    }
+    //Pārbauda lietotāja e-pasta adresi, lai tā atbilstu e-pasta formātam un neiziet no сikla, kamēr e-pasta nav pareiza.
+    //Ja failā ir tas pats e-pasts, tas piedāvās iziet no programmas vai piedāvās vēlreiz ievadīt.
+
+    public void email()
+    {
+        while (true)
+        {
+            bool emailExists = false;
+            Console.WriteLine("Enter your email: ");
+            Email = Console.ReadLine();
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    if (parts.Length > 3 && parts[3] == Email)
+                    {
+                        Console.WriteLine("This email is already in use. \r\nDo you want to cancel your registration?");
+                        string choice = Console.ReadLine();
+                        if (choice.ToLower() == "yes")
+                        {
+                            return;
+                        }
+                        emailExists = true;
+                        break;
+                    }
+                }
+            }
+            if (!emailExists && Regex.IsMatch(Email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"))
+            {
+                file.Writer(Email);
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid email address. Try again!");
+            }
+        }
+    }
+    //Pārbauda lietotāja telefona numuru, lai tas atbilstu telefona numura formātam un neiziet no сikla, kamēr numurs nav pareizs.
+    public void phoneNumber()
+    {
+        do
+        {
+            Console.WriteLine("Enter your phone number: ");
+            PhoneNumber = Console.ReadLine();
+            if (!Regex.IsMatch(PhoneNumber, @"^\+\d+$"))
+            {
+                Console.WriteLine("Invalid phone number. Try again!");
+            }
+            else
+            {
+                file.Writer(Name);
+                break;
+            }
+        } while (true);
+    }
+
+    //Pārbauda lietotāja paroli un neiziet no сikla, kamēr vards nav pareizs.
+    //Tajā būtu vismaz viens lielais burt, mazais burts un cipars. Parolei jābūt vismaz 8 rakstzīmēm garai. 
+    public void password()
+    {
+        do
+        {
+            Console.WriteLine("Enter your password: ");
+            Password = Console.ReadLine();
+            if (!Regex.IsMatch(Password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])\S{8,}$"))
+            {
+                Console.WriteLine("The password must contain at least one uppercase, lowercase letter and number. The password must be at least 8 characters long.");
+            }
+            else
+            { 
+                break;
+            }
+        } while (true);
+
+    }
+    
+    //Pārbauda, vai lietotāja ievadītā parole sakrīt ar iepriekš ievadīto paroli un neiziet no сikla, kamēr otra parole nav pareiza.
+    public void password2()
+    { 
+        do
+        {
+            Console.WriteLine("Enter your password again: ");
+            Password2 = Console.ReadLine();
+            if (Password != Password2)
+            {
+                Console.WriteLine("Passwords do not match. Try again!");
+            }
+            else
+            {
+                file.Writer(Password2);
+                break;
+            }
+        } while (true);
+    }
+}
