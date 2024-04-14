a223d4d7176b76bebeab4ffd00372cbc28d4f16a
22DP1PGrin <131352980+22DP1PGrin@users.noreply.github.com>
2024-04-14 19:39:10 +0300

    Update File.cs

--- File.cs
+++ File.cs
@@ -1,46 +1,68 @@
 using System;
 using System.Text;
 using System.IO;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 public class File
 {
     string email = "";
-    bool PasswordExists = false;
-
-    Card card=new Card();
-
-    string path = @"C:\Users\pgrin\source\repos\Eksamens\ConsoleApp3\Accounts.csv";
-    //string path = @"C:\Users\Admin\source\repos\Eksamens\Eksamens\Accounts.csv";
     
+    Card card = new Card();
+
+    //Metode, kas atrod ceļu uz failu Accounts.csv
+    public string pathToAccountsCsv()
+    {
+        //Iegūstiet darba direktoriju
+        string currentDirectory = Directory.GetCurrentDirectory();
+        
+        // Paceļas par 2 līmeņiem uz augšu, lai sasniegt projekta saknes direktoriju.
+        string projectDirectory = Directory.GetParent(currentDirectory).Parent.Parent.FullName;
+       
+        //Ģenerē pilnu ceļu uz failu Accounts.csv projekta saknē
+        string filePath = Path.Combine(projectDirectory, "Accounts.csv");
+        
+        return filePath;
+    }
+    public string pathToAccountsMap()
+    {
+        //Atrod darba direktoriju
+        string currentDirectory = Directory.GetCurrentDirectory();
+        // Paceļas par 3 līmeņiem uz augšu, lai sasniegtu projekta saknes direktoriju
+        string projectDirectory = Directory.GetParent(Directory.GetParent(Directory.GetParent(currentDirectory).FullName).FullName).FullName;
+        //Ģenerē pilnu ceļu uz failu
+        string filePath = Path.Combine(projectDirectory, "Accounts");
+        
+        return filePath;
+    }
+
     //Metode, kas ieraksta lietotāja datus csv failā.
     public void Writer(string name, string surname, string email, string phoneNumber, string password)
     {
-        try
+        string filePath = pathToAccountsCsv();
+        
+        using (StreamWriter sw = new StreamWriter(filePath, true, Encoding.Default))
         {
-            using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
-            {
-                string line = $"{name},{surname},{email},{phoneNumber},{password}";
-                sw.WriteLine(line);
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
+            string line = $"{name},{surname},{email},{phoneNumber},{password}";
+            sw.WriteLine(line);
         }
     }
-    
-    //Metode, kas izveido kontus csv failos.
+
+    //Metode, kas izveido kontus csv failos veidā.
     public void CreateAccounts()
     {
-        using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+        string filePath = pathToAccountsCsv();
+
+        string filePath2 = pathToAccountsMap();
+        
+        using (StreamReader sr = new StreamReader(filePath, Encoding.Default))
         {
             string line;
+            
             while ((line = sr.ReadLine()) != null)
             {
                 string[] parts = line.Split(',');
-                string email2 = parts[3];
+                string email2 = parts[2];
                 string fileName = $"{email2}.csv";
-                string fullFilePath = Path.Combine(@"C:\Users\pgrin\source\repos\Eksamens\ConsoleApp3\Accounts\", fileName);
+                string fullFilePath = Path.Combine(filePath2, fileName);
+                
                 FileInfo fileInf = new FileInfo(fullFilePath);
 
                 if (!fileInf.Exists)
@@ -55,69 +77,73 @@ public class File
     // Metode, kas pārbauda, vai lietotājs ir reģistrējies.
     public void LogIn()
     {
+        bool PasswordExists = false;
+        
         while (true)
         {
-            Console.WriteLine("To log in, enter your mail and email!\n");
+            CreateAccounts();
+            Console.WriteLine("\nTo log in, enter your email and password!\n");
             Console.WriteLine("Enter your email! ");
             email = Console.ReadLine();
-            try
+            Console.WriteLine("Enter your password: ");
+            string password = Console.ReadLine();
+
+            string filePath = pathToAccountsCsv();
+
+            using (StreamReader sr = new StreamReader(filePath, Encoding.Default))
             {
-                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                string line;
+                
+                while ((line = sr.ReadLine()) != null)
                 {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
+                    string[] parts = line.Split(',');
+                    
+                    if (parts.Contains(email))
                     {
-                        string[] parts = line.Split(',');
-                        if (parts.Contains(email))
+                        if (parts.Contains(password))
                         {
-                            Console.WriteLine("Enter your password: ");
-                            string password = Console.ReadLine();
-                            if (parts.Contains(password))
-                            {
-                                PasswordExists = true;
-                            }
-                            else
-                            {
-                                PasswordExists = false;
-                            }
+                            PasswordExists = true;
+                        }
+                        else
+                        {
+                            PasswordExists = false;
                         }
                     }
                 }
-                if (!PasswordExists)
-                {
-                    Console.WriteLine("Invalid email or password. Try again!\n");
-                    Registration obj = new Registration();
-                    obj.Register();
-                }
-                else
-                {
-
-                    Console.WriteLine("You are logged in!\n");
-                    break;
-                }
-            }
-            catch (Exception e)
+            }      
+            if (!PasswordExists)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine("Invalid email or password. Try again!\n");
+                
+                Registration obj = new Registration();
+                obj.Register();
+            }
+            else
+            {
+                Console.WriteLine("You are logged in!\n");
+                break;
             }
         }
     }
-    
+
     //Metode, kas ieraksta kartes datus kontu failos.
     public void WriteCardInAccounts(string Number, string ExpirationDate, string CVV, double Balance)
     {
 
         string fileName = $"{email}.csv";
-        string fullPath = Path.Combine(@"C:\Users\pgrin\source\repos\Eksamens\ConsoleApp3\Accounts\", fileName);
+        string filePath2 = pathToAccountsMap(); 
+        string fullPath = Path.Combine(filePath2, fileName);
+        
         string lineToFind = "Card data";
         bool CardExist = false;
 
         using (StreamReader sr = new StreamReader(fullPath, Encoding.Default))
         {
             string line;
+            
             while ((line = sr.ReadLine()) != null)
             {
-                foreach (string parts in line.Split(','))
+                foreach (string parts in line.Split(';'))
                 {
                     if (parts.Contains(lineToFind))
                     {
@@ -130,43 +156,105 @@ public class File
         }
         if (!CardExist)
         {
-            try
+            using (StreamWriter sw = new StreamWriter(fullPath, true, Encoding.Default))
             {
-                using (StreamWriter sw = new StreamWriter(fullPath, true, Encoding.Default))
-                {
-                    string line = $"Card data,{Number},{ExpirationDate},{CVV},{Balance}\n";
-                    sw.WriteLine(line);
-                }
+                string line = $"Card data;{Number};{ExpirationDate};{CVV};{Balance}\n";
+                sw.WriteLine(line);
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            card.Balance = Balance;
         }
         else
         {
             card.ChangeCardOrBalance(fullPath, lineToFind);
         }
     }
-    public void WriteShoppingCartInAccounts(List<Product> products)
-{
-    string fileName = $"{email}.csv";
-    string fullPath = Path.Combine(@"C:\Users\pgrin\source\repos\Eksamens\ConsoleApp3\Accounts\", fileName);
 
-    try
+    //Ieraksta preces vajadzīgajā failā, saglabājot kartes datus un nedublējot sarakstus
+    public void WriteShoppingCartInAccounts(List<Product> products)
     {
-        using (StreamWriter sw = new StreamWriter(fullPath, true, Encoding.Default))
+        string fileName = $"{email}.csv";
+        string filePath2 = pathToAccountsMap();
+        string fullPath = Path.Combine(filePath2, fileName);
+        List<string> existingLines = new List<string>();
+        
+        using (StreamReader sr = new StreamReader(fullPath))
         {
+            string line;
+            List<string> updatedLines = new List<string>();
+            
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.StartsWith("Card data"))
+                {
+                    existingLines.Add(line+"\n");
+                }
+            }
+        }
+        using (StreamWriter sw = new StreamWriter(fullPath, false, Encoding.Default))
+        {
+            foreach (var line in existingLines)
+            {
+                sw.WriteLine(line);
+            }
             foreach (var prod in products)
             {
-                string line = $"Shopping cart,{prod.Name},{prod.Price}";
+                string line = $"Shopping cart;{prod.Name};{prod.Price}";
                 sw.WriteLine(line);
+            }
+        } 
+    }
+
+    //Ieraksta produktus no vajadzīgā failā bez kartes datiem
+    public void writeShoppingCardInList(List<Product> products)
+    {
+        string fileName=$"{email}.csv";
+        string filePath2 = pathToAccountsMap();
+        string fullPath = Path.Combine(filePath2, fileName);
+        
+        using (StreamReader sr = new StreamReader(fullPath, Encoding.Default))
+        {
+            string line;
+            
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.StartsWith("Shopping cart"))
+                {
+                    string[] parts = line.Split(';');
+                    string productName = parts[1];
+                    double productPrice;
+                    
+                    if (double.TryParse(parts[2], out productPrice))
+                    {
+                        Product product = new Product(productName, productPrice);
+                        products.Add(product);
+                    }
+                }
             }
         }
     }
-    catch (Exception e)
+    public void Balance()
     {
-        Console.WriteLine(e.Message);
+        string fileNmae = $"{email}.csv";
+        string filePath2 = pathToAccountsMap();
+        string fullPath = Path.Combine(filePath2, fileNmae);
+        using (StreamReader sr = new StreamReader(fullPath, Encoding.Default))
+        {
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                if(line.StartsWith("Card data"))
+                {
+                    string[] parts = line.Split(';');
+                    double balance;
+                    if (double.TryParse(parts[3], out balance))
+                    {
+                        double newBalance = balance;
+                        Console.WriteLine(newBalance);
+                    }
+                }
+
+            }
+        }
     }
-}
 }
