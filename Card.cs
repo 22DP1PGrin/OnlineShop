713d87ff73fa134f288f26989709219828cdbb7b
22DP1PGrin <131352980+22DP1PGrin@users.noreply.github.com>
2024-04-23 10:17:02 +0300

    Update Card.cs

--- Card.cs
+++ Card.cs
@@ -4,7 +4,6 @@ using System.Text.RegularExpressions;
 
 public class Card
 {
-    // kartes dati
     public string CreditCardNumber { get; set; }
     public string CardExpirationDate { get; set; }
     public string CardCVV { get; set; }
@@ -13,36 +12,36 @@ public class Card
 
     public Card()
     {
-        Balance = 0.0; // sākuma bilance
-        CardDataEntered = false; // kartes dati vēl nav ievadītas
+        Balance = 0.0;
+        CardDataEntered = false;
     }
 
+    // Metode, kas ļauj ievadīt kredītkartes datus.
     public void EnterCreditCardData()
     {
         Regex cardNumberRegex = new Regex(@"^(\d{4}\s){3}\d{4}$"); // kartes numura formāts
         Regex expirationDateRegex = new Regex(@"^(0[1-9]|1[0-2])\/\d{2}$"); // derīguma termiņa formāts
         Regex cvvRegex = new Regex(@"^\d{3}$"); // CVV formāts
-        Console.WriteLine("Enter your credit card data:");
-
-        Console.WriteLine("Card number:");
-        Console.WriteLine("For example: 0000 0000 0000 0000");
+        
+        Console.WriteLine("\nEnter your credit card data: ");
+        Console.WriteLine("Card number: ");
+        Console.WriteLine("For example: 0000 0000 0000 0000.");
         CreditCardNumber = Console.ReadLine();
 
         while (!cardNumberRegex.IsMatch(CreditCardNumber))
-          {
-              Console.WriteLine("Wrong format. Please enter card number again:");
-              CreditCardNumber = Console.ReadLine();
-          }
-
+        {
+            Console.WriteLine("Wrong format. Please enter card number again: ");
+            CreditCardNumber = Console.ReadLine();
+        }
         while (true)
         {
-            Console.WriteLine("Card expiration date (MM/YY):");
-            Console.WriteLine("For example: 12/24");
+            Console.WriteLine("Card expiration date (MM/YY): ");
+            Console.WriteLine("For example: 12/24.");
             CardExpirationDate = Console.ReadLine();
 
             if (!expirationDateRegex.IsMatch(CardExpirationDate))
             {
-                Console.WriteLine("Wrong expiration date format. Please enter the expiration date again:");
+                Console.WriteLine("Wrong expiration date format. Please enter the expiration date again: ");
                 continue;
             }
 
@@ -52,25 +51,25 @@ public class Card
 
             if (expYear < 24 || (expYear == 24 && expMonth < 5)) // pārbauda vai karte ir derīga
             {
-                Console.WriteLine("Your card has expired. Please enter a valid expiration date:"); // ja karte nav derīga, ir jāievada derīguma tērmiņu vēlreiz, kamēr nebūs pareizs
+                Console.WriteLine("Your card has expired. Please enter a valid expiration date: "); // ja karte nav derīga, ir jāievada derīguma tērmiņu vēlreiz, kamēr nebūs pareizs
                 continue;
             }
-
             break;
         }
 
         Console.WriteLine("CVV:");
-        Console.WriteLine("For example: 000");
+        Console.WriteLine("For example: 000.");
         CardCVV = Console.ReadLine();
 
         while (!cvvRegex.IsMatch(CardCVV))
         {
-            Console.WriteLine("Wrong format for CVV. Please enter the CVV again:");
+            Console.WriteLine("Wrong format for CVV. Please enter the CVV again: ");
             CardCVV = Console.ReadLine();
         }
     }
 
-    public void SetBalance() // lietotājs pats ievada bilanci
+    // Metode, kas ļauj ievadīt kredītkartes atlikumu.
+    public void SetBalance()
     {
         while (true)
         {
@@ -83,16 +82,17 @@ public class Card
             }
             else
             {
-                Balance = balance;
                 break;
             }
         }
     }
-    public void ChangeCardOrBalance(string FileName, string lineToFind) // ir iespēja mainīt karti un/vai bilanci
+    
+    // Metode, kas ļauj mainīt kredītkartes datus vai atlikumu.
+    public void ChangeCardOrBalance(string FileName, string lineToFind)
     {
         List<string> lines = new List<string>();
 
-        Console.WriteLine("Do you want to change your card and/or its balance?(Y/N)"); // apstiprināt kartes un/vai bilances izmaiņus
+        Console.WriteLine("Do you want to change your card and/or its balance?(Y/N)");
         string choice=Console.ReadLine();
         if (choice.ToLower() == "y")
         {
@@ -105,7 +105,7 @@ public class Card
                 {
                     if (line.Contains(lineToFind))
                     {
-                        lines.Add("Card data," + CreditCardNumber + "," + CardExpirationDate + "," + CardCVV + "," + Balance);
+                        lines.Add("Card data;" + CreditCardNumber + ";" + CardExpirationDate + ";" + CardCVV + ";" + Balance);
                     }
                     else
                     {
