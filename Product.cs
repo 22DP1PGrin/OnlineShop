b15e72bd8cb3b377e31af99263fe7ae0f6d02d33
22DP1PGrin <131352980+22DP1PGrin@users.noreply.github.com>
2024-05-04 19:48:23 +0300

    Done

--- Product.cs
+++ Product.cs
@@ -1,10 +1,9 @@
-using System;
-
 public class Product
 {
     public string Name { get; set; }
     public double Price { get; set; }
 
+    // Konstruktors, kas piešķir vērtības mainīgajiem.
     public Product(string name, double price)
     {
         Name = name;
