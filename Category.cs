fbe114f78ca227a64d680eee8e73e741c729a244
22DP1PGrin <131352980+22DP1PGrin@users.noreply.github.com>
2024-05-04 19:47:31 +0300

    Done

--- Category.cs
+++ Category.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 public class Category
@@ -6,12 +5,14 @@ public class Category
     public string Name { get; set; }
     public List<Product> Products { get; set; }
 
+    // Konstruktors, kas piešķir vērtības mainīgajiem.
     public Category(string name)
     {
         Name = name;
         Products = new List<Product>();
     }
-
+    
+    // Metode, kas ļauj pievienot produktus kategorijai.
     public void AddProduct(Product product)
     {
         Products.Add(product);
